90e1b34d1744abdbb08fe3170e83f0ad79093dbe
Marek Witkowski <mr.marek.witkowski@gmail.com>
2026-04-07 15:28:08 +0200

    test(server): add RSpec tests for Redwing::Server

---
+++ spec/redwing/server_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'redwing/server'
+
+RSpec.describe Redwing::Server do
+  describe '.start' do
+    let(:handler) { double('puma_handler', run: nil) }
+
+    before do
+      allow(Rackup::Handler).to receive(:get).with('puma').and_return(handler)
+    end
+
+    it 'uses puma as the server handler' do
+      Redwing::Server.start(host: 'localhost', port: 9292)
+      expect(Rackup::Handler).to have_received(:get).with('puma')
+    end
+
+    it 'runs with the given host and port' do
+      Redwing::Server.start(host: 'localhost', port: 9292)
+      expect(handler).to have_received(:run).with(anything, Host: 'localhost', Port: 9292)
+    end
+  end
+end
