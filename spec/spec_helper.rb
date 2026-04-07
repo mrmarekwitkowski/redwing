0be3b8a0db44d0bab49c3b4094c5805c96c10b23
Marek Witkowski <mr.marek.witkowski@gmail.com>
2026-04-07 15:15:11 +0200

    feat(rspec): add RSpec baseline layout
    - creates RSpec HTML report
    - adds SimpleCov to RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'redwing'
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  coverage_dir 'tmp/coverage'
+  add_filter '/spec/'
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
