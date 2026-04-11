1ba1c832c4c774353534ae95493e4588f0cc7ca4
Marek Witkowski <mr.marek.witkowski@gmail.com>
2026-04-11 19:35:02 +0200

    feat(router): support GET, POST, PUT, PATCH and DELETE request methods
    - adds SUPPORTED_METHODS constants to router

--- lib/redwing/router.rb
+++ lib/redwing/router.rb
@@ -2,14 +2,18 @@
 
 module Redwing
   class Router
+    SUPPORTED_METHODS = %w[GET POST PUT PATCH DELETE].freeze
+
     attr_reader :routes
 
     def initialize
       @routes = []
     end
 
-    def get(path, &block)
-      @routes << {method: 'GET', path: path, handler: block}
+    SUPPORTED_METHODS.each do |verb|
+      define_method(verb.downcase) do |path, &block|
+        @routes << {method: verb, path: path, handler: block}
+      end
     end
 
     def match(method, path)
