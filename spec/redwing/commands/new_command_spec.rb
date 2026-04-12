957e48f2bdefe5b3afd82089bcc4d0ae5d6ee2fb
Marek Witkowski <mr.marek.witkowski@gmail.com>
2026-04-12 12:14:20 +0200

    feat: simplify redwing new to minimal API app
    - Remove --api flag; new apps are API-only by default
    - Generate StatusController with GET /status
    - Add home_controller.rb template for future scaffold use
    - Use single quotes in generated templates

--- spec/redwing/commands/new_command_spec.rb
+++ spec/redwing/commands/new_command_spec.rb
@@ -8,37 +8,13 @@ RSpec.describe Redwing::Commands::NewCommand do
 
     let(:target_path) { "#{Pathname.pwd}/my-app" }
 
-    shared_examples 'scaffolds base templates' do
-      it 'generates all base templates' do
-        %w[README.md Gemfile config/routes.rb].each do |file|
-          expect(Redwing::Generator).to have_received(:create_file_by_template)
-            .with("templates/#{file}.tt", "#{target_path}/#{file}", {name: 'my-app'})
-        end
-      end
-    end
-
-    context 'without --api flag' do
+    context 'generates a minimal API app' do
       before { described_class.new(%w[my-app], {}).perform }
 
-      include_examples 'scaffolds base templates'
-
-      it 'generates view templates' do
-        %w[app/views/layouts/application.html.erb app/views/home/index.html.erb].each do |file|
+      it 'generates all templates' do
+        %w[README.md Gemfile config/routes.rb app/controllers/status_controller.rb].each do |file|
           expect(Redwing::Generator).to have_received(:create_file_by_template)
             .with("templates/#{file}.tt", "#{target_path}/#{file}", {name: 'my-app'})
-        end
-      end
-    end
-
-    context 'with --api flag' do
-      before { described_class.new(%w[my-app], {'api' => true}).perform }
-
-      include_examples 'scaffolds base templates'
-
-      it 'does not generate view templates' do
-        %w[app/views/layouts/application.html.erb app/views/home/index.html.erb].each do |file|
-          expect(Redwing::Generator).not_to have_received(:create_file_by_template)
-            .with("templates/#{file}.tt", anything, anything)
         end
       end
     end
