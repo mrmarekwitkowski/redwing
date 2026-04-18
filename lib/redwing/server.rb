27e6f7c11916f8d75bf02c5bd48e9e34d42f641d
Marek Witkowski <mr.marek.witkowski@gmail.com>
2026-04-18 14:52:33 +0200

    refactor(server): route first, static as GET-only fallback
    - router runs before static; routes now win over same-named files in public/
    - Rack::Static only consulted on GET/HEAD route miss, removing the 405
      hack the GetOnlyStatic wrapper was papering over
    - drop GetOnlyStatic class and the Rack::Builder wrap
    - non-GET unmatched requests return 404 directly

--- lib/redwing/server.rb
+++ lib/redwing/server.rb
@@ -7,57 +7,42 @@ require 'redwing'
 
 module Redwing
   module Server
-    class GetOnlyStatic
-      def initialize(app, **opts)
-        @static = Rack::Static.new(app, **opts)
-        @app = app
-      end
-
-      def call(env)
-        if %w[GET HEAD].include?(env['REQUEST_METHOD'])
-          @static.call(env)
-        else
-          @app.call(env)
-        end
-      end
-    end
-
     def self.start(host:, port:)
       Redwing.load_controllers
+      not_found = [404, {'content-type' => 'application/json'}, ['{"error":"Not Found"}']]
+
+      static = Rack::Static.new(
+        ->(_env) { not_found },
+        urls: [''], root: Redwing.config.public_root, cascade: true
+      )
 
       app = proc do |env|
         request = Rack::Request.new(env)
         match = Redwing.routes.match(request.request_method, request.path_info)
 
-        if match
-          dispatcher = Redwing::Dispatcher.new
-          body = dispatcher.call(match[:route], request, match[:params])
+        response =
+          if match
+            dispatcher = Redwing::Dispatcher.new
+            body = dispatcher.call(match[:route], request, match[:params])
 
-          response = case body
-                     when Hash
-                       [200, {'content-type' => 'application/json'}, [body.to_json]]
-                     when String
-                       [200, {'content-type' => 'text/html; charset=utf-8'}, [body]]
-                     else
-                       raise Redwing::Error::InvalidResponse,
-                             "Route handler must return a Hash or String, got #{body.class}"
-                     end
+            case body
+            when Hash   then [200, {'content-type' => 'application/json'}, [body.to_json]]
+            when String then [200, {'content-type' => 'text/html; charset=utf-8'}, [body]]
+            else raise Redwing::Error::InvalidResponse,
+                       "Route handler must return a Hash or String, got #{body.class}"
+            end
+          elsif %w[GET HEAD].include?(request.request_method)
+            static.call(env)
+          else
+            not_found
+          end
 
-          Redwing.config.logger.info("#{request.request_method} #{request.path_info} => #{response[0]}")
-          response
-        else
-          Redwing.config.logger.info("#{request.request_method} #{request.path_info} => 404")
-          [404, {'content-type' => 'application/json'}, ['{"error":"Not Found"}']]
-        end
-      end
-
-      wrapped = Rack::Builder.new do
-        use GetOnlyStatic, urls: [''], root: Redwing.config.public_root, cascade: true
-        run app
+        Redwing.config.logger.info("#{request.request_method} #{request.path_info} => #{response[0]}")
+        response
       end
 
       handler = Rackup::Handler.get('puma')
-      handler.run(wrapped, Host: host, Port: port)
+      handler.run(app, Host: host, Port: port)
     end
   end
 end
